ca7854bada37836f00166d53149c83cd81d2a4cd
filipenevola <filipenevola@gmail.com>
2021-02-25 17:25:50 -0400

    Replacing React class by function component with hooks

--- README.md
+++ README.md
@@ -4,6 +4,25 @@ App to manage Movies that you want to Watch.
 
 I created this project to teach Meteor, React, PWA in [Setec18](https://www.even3.com.br/setec18) in Brazil.
 
-## How to 
+## How to install
+
+```bash
+meteor npm install -g yarn
+meteor yarn install
+```
+
+## How to run
+
+```bash
+meteor yarn start
+```
+
+## How to deploy
+
+```bash
+meteor deploy <select a subdomain>.meteorapp.com --free --mongo
+```
+
+[Read more](https://cloud-guide.meteor.com/deploy-quickstart.html)
 
 ## License: MIT
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,225 +1,190 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { methodCall } from "./methods";
 import { getImageUrl } from "../api/moviesHelper";
 
 const MAX_OVERVIEW_LENGTH = 250;
 
-export class App extends Component {
-  state = {
-    showMyMovies: false,
-    movies: [],
-    moviesIds: [],
-    search: "",
-    moviesSearch: [],
-    moviesSearchTotal: 0
-  };
+export const App = () => {
+  const [showMyMovies, setShowMyMovies] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [moviesSearch, setMoviesSearch] = useState([]);
 
-  clearMoviesSearch = () =>
-    this.setState(() => ({
-      moviesSearch: []
-    }));
+  const clearMoviesSearch = () => setMoviesSearch([]);
 
-  searchMovies = ({ target: { value } }) => {
-    this.setState(() => ({
-      search: value
-    }));
+  const searchMovies = ({ target: { value } }) => {
+    setSearch(value);
     if (!value) {
-      this.loadPopularMovies();
+      loadPopularMovies();
       return;
     }
     methodCall("moviesSearch", value)
       .then(result => {
         const data = JSON.parse(result);
         if (!data || !data.results) {
-          this.clearMoviesSearch();
+          clearMoviesSearch();
           return;
         }
-        const {
-          results: moviesSearch,
-          total_results: moviesSearchTotal
-        } = data;
-        this.setState(() => ({
-          moviesSearch,
-          moviesSearchTotal
-        }));
+        const { results: moviesSearch } = data;
+        setMoviesSearch(moviesSearch);
       })
       .catch(error => {
         console.error(error);
-        this.clearMoviesSearch();
+        clearMoviesSearch();
       });
   };
 
-  saveMovie = movie => {
-    methodCall("movieSave", movie);
-    this.setState(({ movies, moviesIds }) => {
-      if (moviesIds.includes(movie.id)) return {};
-      return {
-        movies: [...movies, movie],
-        moviesIds: [...moviesIds, movie.id]
-      };
-    });
+  const saveMovie = movie => {
+    methodCall("movieSave", movie).catch(error =>
+      console.error(`Error saving a movie ${movie.id}`,error)
+    );
+    if (!movies.map(({ id }) => id).includes(movie.id)) {
+      setMovies([...movies, movie]);
+    }
   };
 
-  removeMovie = movie => {
-    methodCall("movieRemove", movie);
-    this.setState(({ movies, moviesIds }) => {
-      if (!moviesIds.includes(movie.id)) return {};
-      return {
-        movies: movies.filter(m => m.id !== movie.id),
-        moviesIds: moviesIds.filter(id => id !== movie.id)
-      };
-    });
+  const removeMovie = movie => {
+    methodCall("movieRemove", movie).catch(error =>
+      console.error(`Error removing a movie ${movie.id}`,error)
+    );
+    if (movies.map(({ id }) => id).includes(movie.id)) {
+      setMovies(movies.filter(m => m.id !== movie.id));
+    }
   };
 
-  loadPopularMovies = () => {
-    this.setState(() => ({
-      moviesSearch: []
-    }));
+  const loadPopularMovies = () => {
+    setMoviesSearch([]);
     methodCall("moviesPopular").then(result => {
       const data = JSON.parse(result);
       if (!data || !data.results) {
         return;
       }
       const { results: moviesSearch } = data;
-      this.setState(() => ({
-        moviesSearch
-      }));
+      setMoviesSearch(moviesSearch);
     });
   };
 
-  toggleShowMyMovies = () =>
-    this.setState(
-      ({ showMyMovies }) => ({
-        showMyMovies: !showMyMovies,
-        search: ""
-      }),
-      () => !this.state.showMyMovies && this.loadPopularMovies()
-    );
+  const toggleShowMyMovies = () => {
+    if (showMyMovies) {
+      loadPopularMovies();
+    }
+    setShowMyMovies(!showMyMovies);
+    setSearch("");
+  };
 
-  componentDidMount() {
-    methodCall("movies").then(movies =>
-      this.setState(() => ({ movies, moviesIds: movies.map(m => m.id) }))
-    );
-    this.loadPopularMovies();
-  }
+  useEffect(() => {
+    methodCall("movies").then(movies => {
+      setMovies(movies);
+    });
+    loadPopularMovies();
+  }, []);
 
-  render() {
-    const movies = this.state.showMyMovies
-      ? this.state.movies
-      : this.state.moviesSearch;
-    return (
-      <div className="app">
-        <header>
-          <div className="app-bar">
-            <div className="app-header">
-              <h1>
-                Wantch:{" "}
-                {this.state.showMyMovies &&
-                  `My Movies (${this.state.movies.length})`}
-                {!this.state.showMyMovies &&
-                  !this.state.search &&
-                  "Popular Movies"}
-                {!this.state.showMyMovies &&
-                  this.state.search &&
-                  "Search Movies"}
-              </h1>
-            </div>
-            <div>
-              <button className="add" onClick={() => this.toggleShowMyMovies()}>
-                {this.state.showMyMovies
-                  ? "See Popular"
-                  : `See My (${this.state.movies.length})`}
-              </button>
-            </div>
+  const moviesToShow = showMyMovies ? movies : moviesSearch;
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>
+              Wantch: {showMyMovies && `My Movies (${movies.length})`}
+              {!showMyMovies && !search && "Popular Movies"}
+              {!showMyMovies && search && "Search Movies"}
+            </h1>
           </div>
-
-          <div className="movie-search">
-            <div>
-              <i className="material-icons">search</i>
-            </div>
-            <div className="movie-search-text">
-              <input
-                disabled={this.state.showMyMovies}
-                type="text"
-                value={this.state.search}
-                placeholder={
-                  this.state.showMyMovies
-                    ? "Search disabled on My Movies"
-                    : "Type the movie that you want to watch..."
-                }
-                onChange={this.searchMovies}
-              />
-            </div>
+          <div>
+            <button className="add" onClick={() => toggleShowMyMovies()}>
+              {showMyMovies ? "See Popular" : `See My (${movies.length})`}
+            </button>
           </div>
-        </header>
+        </div>
 
-        <div className="main">
-          <div className="movie-list">
-            {movies.map((movie, index) => {
-              const {
-                id,
-                title,
-                vote_average: voteAverage,
-                poster_path: posterPath,
-                overview
-              } = movie;
-              const voteClassName =
-                voteAverage < 5 ? "bad" : voteAverage > 7 ? "good" : "default";
-              const voteAverageFormatted = !voteAverage
-                ? "0.0"
-                : voteAverage === 10
-                  ? "10"
-                  : voteAverage.toFixed(1);
-              const overviewFormatted =
-                overview && overview.length > MAX_OVERVIEW_LENGTH
-                  ? `${overview.substring(0, MAX_OVERVIEW_LENGTH)}...`
-                  : overview;
-              return (
-                <div key={id} className="movie-item">
-                  <div className="movie">
-                    <div className="movie-image">
-                      <img src={getImageUrl(posterPath)} />
+        <div className="movie-search">
+          <div>
+            <i className="material-icons">search</i>
+          </div>
+          <div className="movie-search-text">
+            <input
+              disabled={showMyMovies}
+              type="text"
+              value={search}
+              placeholder={
+                showMyMovies
+                  ? "Search disabled on My Movies"
+                  : "Type the movie that you want to watch..."
+              }
+              onChange={searchMovies}
+            />
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <div className="movie-list">
+          {moviesToShow.map((movie, index) => {
+            const {
+              id,
+              title,
+              vote_average: voteAverage,
+              poster_path: posterPath,
+              overview
+            } = movie;
+            const voteClassName =
+              voteAverage < 5 ? "bad" : voteAverage > 7 ? "good" : "default";
+            const voteAverageFormatted = !voteAverage
+              ? "0.0"
+              : voteAverage === 10
+                ? "10"
+                : voteAverage.toFixed(1);
+            const overviewFormatted =
+              overview && overview.length > MAX_OVERVIEW_LENGTH
+                ? `${overview.substring(0, MAX_OVERVIEW_LENGTH)}...`
+                : overview;
+            return (
+              <div key={id} className="movie-item">
+                <div className="movie">
+                  <div className="movie-image">
+                    <img
+                      alt={`Poster of ${title}`}
+                      src={getImageUrl(posterPath)}
+                    />
+                  </div>
+
+                  <div className="movie-content">
+                    <div className="movie-title">
+                      <strong>{title}</strong>
                     </div>
-
-                    <div className="movie-content">
-                      <div className="movie-title">
-                        <strong>{title}</strong>
-                      </div>
-                      <div className="movie-description">
-                        {overviewFormatted}
-                      </div>
-                      <div className="movie-footer">
-                        <div className="movie-vote">
-                          <div className={voteClassName}>
-                            {voteAverageFormatted}
-                          </div>
+                    <div className="movie-description">{overviewFormatted}</div>
+                    <div className="movie-footer">
+                      <div className="movie-vote">
+                        <div className={voteClassName}>
+                          {voteAverageFormatted}
                         </div>
-                        <div className="movie-actions">
-                          {this.state.moviesIds.includes(movie.id) ? (
-                            <button
-                              className="remove"
-                              onClick={() => this.removeMovie(movie)}
-                            >
-                              remove, please!
-                            </button>
-                          ) : (
-                            <button
-                              className="add"
-                              onClick={() => this.saveMovie(movie)}
-                            >
-                              {index % 2 === 0 ? "want watch!" : "I wantch!"}
-                            </button>
-                          )}
-                        </div>
+                      </div>
+                      <div className="movie-actions">
+                        {movies.map(({ id }) => id).includes(movie.id) ? (
+                          <button
+                            className="remove"
+                            onClick={() => removeMovie(movie)}
+                          >
+                            remove, please!
+                          </button>
+                        ) : (
+                          <button
+                            className="add"
+                            onClick={() => saveMovie(movie)}
+                          >
+                            {index % 2 === 0 ? "want watch!" : "I wantch!"}
+                          </button>
+                        )}
                       </div>
                     </div>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
