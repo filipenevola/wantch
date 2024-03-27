1fcb1a3d734633112a5e29999b9b0cfbcdac9a6a
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-03-27 10:56:18 -0300

    To meteor 3

--- imports/data/MoviesCollection.js
+++ imports/data/MoviesCollection.js
@@ -3,14 +3,14 @@ import { Mongo } from "meteor/mongo";
 const moviesCollection = new Mongo.Collection("movies");
 
 Object.assign(moviesCollection, {
-  save(movie) {
-    const dbMovie = this.findOne({ id: +movie.id });
+  async save(movie) {
+    const dbMovie = await this.findOneAsync({ id: +movie.id });
     if (dbMovie) {
-      this.update(dbMovie._id, movie);
-      return this.findOne(dbMovie._id);
+      await this.updateAsync(dbMovie._id, movie);
+      return this.findOneAsync(dbMovie._id);
     }
-    return this.findOne(this.insert(movie));
-  }
+    return this.findOneAsync(await this.insertAsync(movie));
+  },
 });
 
 export { moviesCollection as MoviesCollection };
